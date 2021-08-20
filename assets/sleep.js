8e83f06339a93d8100893208598a3bdd71ebf524
Arcy <a@rqft.space>
2021-08-20 15:56:55 -0500

    a

---
+++ assets/sleep.js
@@ -0,0 +1,8 @@
+/**
+ * Waits for some amount of time
+ * @param {number} ms
+ * @returns
+ */
+function sleep(ms) {
+  return setTimeout(undefined, ms);
+}
--- clock/index.js
+++ clock/index.js
@@ -1,22 +1,30 @@
 function updateTime() {
-    const time = new Date()
-    const timeEl = document.getElementById('time')
-    const dateEl = document.getElementById('date')
-    const waitingEl = document.getElementById('waiting')
-    /**
-     * @param {number} a 
-     * @param {number} n
-     * @returns 
-     */
-    function toc(a, n = 2) { return a.toString(16).padStart(n, '0') }
-    
-    const color = parseInt(toc(time.getHours()) + toc(time.getMinutes()) + toc(time.getSeconds()), 16) + 0x080000
-    document.body.style.backgroundColor = `#${color.toString(16)}`
-    timeEl.innerText = `${time.toLocaleTimeString(undefined, { hour12: false })} #${color.toString(16)}`
-    dateEl.innerText = time.toLocaleDateString()
-    const textCol = color > 0x888888 ? 'black' : 'white'
-    timeEl.style.color = textCol
-    dateEl.style.color = textCol
-    waitingEl.style.color = textCol
+  const time = new Date();
+  const timeEl = document.getElementById("time");
+  const dateEl = document.getElementById("date");
+  const waitingEl = document.getElementById("waiting");
+  /**
+   * @param {number} a
+   * @param {number} n
+   * @returns
+   */
+  function toc(a, n = 2) {
+    return a.toString(16).padStart(n, "0");
+  }
+
+  const color =
+    parseInt(
+      toc(time.getHours()) + toc(time.getMinutes()) + toc(time.getSeconds()),
+      16
+    ) + 0x080000;
+  document.body.style.backgroundColor = `#${color.toString(16)}`;
+  timeEl.innerText = `${time.toLocaleTimeString(undefined, {
+    hour12: false,
+  })} #${color.toString(16)}`;
+  dateEl.innerText = time.toLocaleDateString();
+  const textCol = color > 0x888888 ? "black" : "white";
+  timeEl.style.color = textCol;
+  dateEl.style.color = textCol;
+  waitingEl.style.color = textCol;
 }
-setInterval(updateTime, 1)
+setInterval(updateTime, 1);
