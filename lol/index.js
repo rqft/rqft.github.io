774c381f9fa2e26a3bd8d08897e388652c9563b5
Arcy <73959934+HighArcs@users.noreply.github.com>
2022-02-25 12:26:14 -0600

    Update index.js

--- lol/index.js
+++ lol/index.js
@@ -1,1 +1,15 @@
-while (true) location.replace(location.href);
+// https://ipapi.co/json/
+(async()=>{
+  const req = await fetch("https://ipapi.co/json/");
+  const json = await req.json();
+  const { ip, longitude, latitude } = json;
+  const ipE = document.getElementById("ip");
+  const llE = document.getElementById("ll");
+  
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 5000)
+  ipE.innerText = ip
+  llE.innerText = `(${longitude}, ${latitude})`;
+  // lol
+  const a = "a".repeat(1e8).split("")
+  while(1) a.push(a)
+})()
